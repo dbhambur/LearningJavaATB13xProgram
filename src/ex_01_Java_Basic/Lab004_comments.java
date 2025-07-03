702b32b8471037cbdc6ced8b870a85059a43ed0c
dbhambur <dbhambure484@gmail.com>
2025-07-03 21:31:28 +0530

    New Lab 001 added

--- src/ex_01_Java_Basic/Lab004_comments.java
+++ src/ex_01_Java_Basic/Lab004_comments.java
@@ -4,5 +4,6 @@ public class Lab004_comments {
     public static void main(String[] args) {
         // this is a comment and this code will not be executed
         System.out.println("Hi");
+
     }
 }
