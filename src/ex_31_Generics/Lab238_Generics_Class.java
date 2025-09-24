01387de95f631498648e0f7ee0ab73631b2fe6d7
dbhambur <dbhambure484@gmail.com>
2025-09-24 11:35:24 +0530

    LAB EXERCISESa

---
+++ src/ex_31_Generics/Lab238_Generics_Class.java
@@ -0,0 +1,26 @@
+package ex_31_Generics;
+
+public class Lab238_Generics_Class {
+    public static void main(String[] args) {
+        GenericClass g = new GenericClass(10);
+        GenericClass g1 = new GenericClass("Pramod");
+        GenericClass g2 = new GenericClass("true");
+        GenericClass g3 = new GenericClass(10.45);
+    }
+} // T is a placeholder, it can be any data type
+
+class GenericClass <T>{
+    private T data;
+
+    public GenericClass(T data) {
+        this.data = data;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+}
